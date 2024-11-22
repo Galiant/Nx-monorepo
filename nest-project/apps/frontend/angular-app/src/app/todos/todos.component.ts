e5234a945587dad43c9c4ca447949e2ff99d8c88
Antonijo Galic <antonijo.galic@and.digital>
2024-11-22 16:51:12 +0100

    Adding todos list using external api

--- nest-project/apps/frontend/angular-app/src/app/todos/todos.component.ts
+++ nest-project/apps/frontend/angular-app/src/app/todos/todos.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { Todo } from '../model/todo.type';
+import { TodosService } from '../services/todos.service';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
   imports: [],
   templateUrl: './todos.component.html',
-  styleUrl: './todos.component.css'
+  styleUrl: './todos.component.scss',
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit {
+  todoService = inject(TodosService);
+  todoItems = signal<Array<Todo>>([]);
 
+  ngOnInit(): void {
+    this.todoService
+      .getTodosFromApi()
+      .pipe(
+        catchError((error) => {
+          console.log(error);
+          throw error;
+        })
+      )
+      .subscribe((todos) => {
+        this.todoItems.set(todos);
+      });
+  }
 }
