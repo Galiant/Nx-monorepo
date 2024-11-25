87b2e85c4ae702ef30d3c63c8dda9d8aaceaf2ee
Antonijo Galic <antonijo.galic@and.digital>
2024-11-25 12:12:02 +0100

    Highliting todos and filtering through search term

--- nest-project/apps/backend/nestjs-task-management/src/main.ts
+++ nest-project/apps/backend/nestjs-task-management/src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
+  app.enableCors();
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT ?? 3000;
--- nest-project/apps/frontend/angular-app/src/app/components/todo-item/todo-item.component.ts
+++ nest-project/apps/frontend/angular-app/src/app/components/todo-item/todo-item.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { HighlightCompletedTodoDirective } from '../../directives/highlight-completed-todo.directive';
+import { Todo } from '../../model/todo.type';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-todo-item',
   standalone: true,
-  imports: [],
+  imports: [HighlightCompletedTodoDirective, UpperCasePipe],
   templateUrl: './todo-item.component.html',
-  styleUrl: './todo-item.component.css'
+  styleUrl: './todo-item.component.css',
 })
 export class TodoItemComponent {
+  todo = input.required<Todo>();
+  todoToggled = output<Todo>();
 
+  todoClicked() {
+    this.todoToggled.emit(this.todo());
+  }
 }
